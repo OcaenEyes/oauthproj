a4781f105f92d9dd2629fc43c887f51796429edc
OcaenEyes <1450136519@qq.com>
2020-03-27 23:36:34 +0800

    master

---
+++ oauth-weipo/src/main/java/com/gzy/oauthweipo/OauthWeipoApplication.java
@@ -0,0 +1,13 @@
+package com.gzy.oauthweipo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthWeipoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OauthWeipoApplication.class, args);
+    }
+
+}
---
+++ oauth-weipo/src/test/java/com/gzy/oauthweipo/OauthWeipoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gzy.oauthweipo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthWeipoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
